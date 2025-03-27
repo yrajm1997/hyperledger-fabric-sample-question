37ec033450c0c6d73277e9dfab89794b2e769023
Yograj Mahawar <89453936+yrajm1997@users.noreply.github.com>
2025-03-27 19:54:27 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ REPO_URL="https://github.com/MLOPS-test/hyperledger-fabric-sample-question.git"
 TARGET_DIR="$(pwd)"
 
 # Clone the repository
-echo "Cloning repository from $REPO_URL..."
+echo "Cloning repository from $REPO_URL to $TARGET_DIR ..."
 git clone $REPO_URL $TARGET_DIR
 
 # Check if clone was successful
